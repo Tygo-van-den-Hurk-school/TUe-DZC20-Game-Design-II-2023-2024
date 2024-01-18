fa785f9b178ac9e368f51b04564c9189b369443b
JelteSchuurmans <j.schuurmans1999@gmail.com>
2024-01-18 15:29:43 +0100

    final
    please be done

--- Global Scripts/GameData.gd
+++ Global Scripts/GameData.gd
@@ -2,3 +2,9 @@ extends Node
 
 var selectedBlock = 1
 var physicsMode = false
+var current_grid
+var popupVisible = false
+
+func _input(event):
+	if event.is_action_pressed("escape"):
+		get_tree().quit()
--- scenes/popupscreen/popupscreen.gd
+++ scenes/popupscreen/popupscreen.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+var on = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Timer.start()
 	visible = false
 	pass # Replace with function body.
 
@@ -10,3 +12,27 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_timer_timeout():
+	if on:
+		self.visible = true
+		on = false
+	else:
+		self.visible = false
+		on = true
+	var value = randi_range(1,3)
+	if value == 1:
+		$DialogBox/LevelTitle.text = "Material Selection"
+		$DialogBox/LevelInfo.text = "Choose materials suited to the building's environment and purpose. Factors like tensile strength, durability, and resistance to elements are crucial"
+	elif value == 2:
+		$DialogBox/LevelTitle.text = "Load Analysis"
+		$DialogBox/LevelInfo.text = "Accurately calculate and distribute loads, including dead loads, live loads, and environmental pressures, to ensure stability and safety."
+	elif value == 3:
+		$DialogBox/LevelTitle.text = "Sustainable Materials"
+		$DialogBox/LevelInfo.text = "Choose eco-friendly materials for construction, promoting environmental sustainability and long-term efficiency."
+	$Timer.start()
+
+
+func _on_button_label_pressed():
+	self.visible = false
